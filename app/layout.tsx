7fe6beea2bb6d7037ef033896d59f18566f36758
frizaski <frizaskialfath28@gmail.com>
2026-06-19 15:25:09 +0700

    Updated: Favicon ICO

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
       <head>
         <title>VoxHaul</title>
         <meta name="description" content="Marketplace Creativox" />
+        {/* Favicon configuration */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/Assets/Favicon%20ICO/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/Assets/Favicon%20ICO/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/Assets/Favicon%20ICO/favicon-16x16.png" />
+        <link rel="manifest" href="/Assets/Favicon%20ICO/site.webmanifest" />
+        <link rel="shortcut icon" href="/Assets/Favicon%20ICO/favicon.ico" />
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
